6a084c28d1520e5f4b7959f0c018b9dffa40b995
soaadhallak <soaadhallak2@gmail.com>
2026-07-26 10:30:31 +0300

    Remove password complexity validation from CreateUserDto to simplify user creation process.

--- src/module/users/application/dto/create-user.dto.ts
+++ src/module/users/application/dto/create-user.dto.ts
@@ -6,10 +6,6 @@ export class CreateUserDto {
 
   @IsString()
   @MinLength(8)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
-    message:
-      'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character',
-  })
   password: string;
 
   @IsInt()
